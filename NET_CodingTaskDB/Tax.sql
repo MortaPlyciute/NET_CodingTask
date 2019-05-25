2e6831654cb3785b9cf2204eec2fbfd7431756a2
MortaPlyciute <morta.plyciuraityte@gmail.com>
2019-05-25 15:57:52 +0300

    Added: DB project (fixed reference)

---
+++ NET_CodingTaskDB/Tax.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tax]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [StartDate] DATE NOT NULL, 
+    [EndDate] DATE NULL, 
+    [Value] DECIMAL(10, 4) NOT NULL, 
+    [Type] NCHAR(1) NOT NULL
+)
